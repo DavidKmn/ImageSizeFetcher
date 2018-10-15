0e9b189063118fc0adcd1e52935e7b18c87e2195
DavidKmn <davidk.9889@gmail.com>
2018-10-15 14:23:35 +0100

    Initial Commit

---
+++ ImageSizeFetcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageSizeFetcher
+//
+//  Created by David on 15/10/2018.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
